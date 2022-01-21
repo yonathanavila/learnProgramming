29dd9df21bdd44904c16e2e9f201cfaaa92db264
Yonathan Cruz <yonathancruz2019@gmail.com>
2022-01-21 15:46:18 -0600

    Integracin de Dgraph a backend

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/ycruz
 
 go 1.17
+
+require (
+	github.com/dgraph-io/dgo/v200 v200.0.0-20210401091508-95bfd74de60e // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20220121210141-e204ce36a2ba // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220118154757-00ab72f36ad5 // indirect
+	google.golang.org/grpc v1.43.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"encoding/base64"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+func redirectPolicyFunc(req *http.Request, via []*http.Request) error {
+	req.Header.Add("Authorization", "Basic "+basicAuth("username1", "password123"))
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	url := "https://icanhazdadjoke.com/"
+
+	client := &http.Client{
+		CheckRedirect: redirectPolicyFunc,
+	}
+
+	resp, err := client.Get(url)
+	fmt.Println("\nAPI Joke1: ", resp)
+
+	req, err := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("User-Agent", "Dadjoke CLI (github.com/yonathanavila)")
+
+	resp, err = client.Do(req)
+
+	if err != nil {
+		// handle error
+		log.Println("Ocurrio un error: ", err)
+	}
+	fmt.Println("\nAPI Joke2: ", resp)
+
+	defer resp.Body.Close()
+
+	fmt.Println("\nAPI Joke3: ", resp)
 }
